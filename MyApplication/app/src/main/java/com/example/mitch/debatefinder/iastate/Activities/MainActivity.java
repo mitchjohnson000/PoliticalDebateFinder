5f2438c554ee20877f1273eb80b911160380204e
Mitch Johnson <Mitch Johnson>
2017-03-09 01:48:33 -0600

    inital commit

---
+++ MyApplication/app/src/main/java/com/example/mitch/debatefinder/iastate/Activities/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mitch.debatefinder.iastate.Activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by mitch on 3/9/2017.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+}
