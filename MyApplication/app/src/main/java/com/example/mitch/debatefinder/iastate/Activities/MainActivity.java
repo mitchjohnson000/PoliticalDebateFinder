fb45850ff95620bbac68048e36ae8e3f92363eb0
Mitch Johnson <Mitch Johnson>
2017-03-09 02:24:36 -0600

    login page pretty good

--- MyApplication/app/src/main/java/com/example/mitch/debatefinder/iastate/Activities/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/mitch/debatefinder/iastate/Activities/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.mitch.debatefinder.iastate.Activities;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.mitch.debatefinder.R;
 
 /**
  * Created by mitch on 3/9/2017.
@@ -13,9 +16,29 @@ import android.view.View;
 
 public class MainActivity extends Activity{
 
+    public EditText login;
+    public EditText password;
+    public TextView register;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        //inits
+        login = (EditText) findViewById(R.id.login_box);
+        password = (EditText) findViewById(R.id.password_box);
+        register = (TextView) findViewById(R.id.register_box);
+        //onClickListeners
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
 
     }
 
